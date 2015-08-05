ee1d47ce0b488ba069456883df2e29db371fb013
clutchs <muh.hilman@gmail.com>
2015-08-05 18:43:38 +0700

    kerangka luhut

--- html/js/general.js
+++ html/js/general.js
@@ -1,5 +1,3 @@
 $(document).ready(function() {
-    $('#Carousel').carousel({
-        interval: 5000
-    })
+
 });
--- html/submekan.php
+++
@@ -1,4 +0,0 @@
-<ul class="nav navbar-nav vag-font">
-	<li><a href="mekanisme_viafb.php">Via Facebook</a></li>
-	<li><a href="mekanisme_viapos.php">Via Pos</a></li>
-</ul>
